a243337cede971c3370a33ee5559f2a2cb8c3bf0
Olubukola Martins <19bukola97@gmail.com>
2022-10-15 22:17:01 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,77 @@
 import "./styles.css";
+// import React from "react";
+import { useState } from "react";
 
 export default function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+  const ops = ["*", "/", "-", "+", "."];
+  const updateCalc = (value) => {
+    if (
+      (ops.includes(value) && calc === "") ||
+      (ops.includes(value) && ops.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+
+    setCalc(calc + value);
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  };
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  };
+
+  const del = () => {
+    if (calc === "") {
+      return;
+    }
+    const value = calc.slice(0, -1);
+    setCalc(value);
+  };
+
+  const clear = () => {
+    if (calc === "") {
+      return;
+    }
+    const value = "0";
+    setCalc(value);
+    setResult("0");
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className="calculator">
+        <div className="display">
+          {result ? <span> ({result}) </span> : " "}
+          {calc || "0"}
+        </div>
+
+        <div className="operators">
+          <button onClick={() => updateCalc("*")}> * </button>
+          <button onClick={() => updateCalc("/")}> / </button>
+          <button onClick={() => updateCalc("-")}> - </button>
+          <button onClick={() => updateCalc("+")}> + </button>
+          <button onClick={calculate}> = </button>
+          <button onClick={del}> DEL </button>
+        </div>
+
+        <div className="numbers">
+          <button onClick={() => updateCalc("0")}> 0 </button>
+          <button onClick={() => updateCalc("1")}> 1 </button>
+          <button onClick={() => updateCalc("2")}> 2 </button>
+          <button onClick={() => updateCalc("3")}> 3 </button>
+          <button onClick={() => updateCalc("4")}> 4 </button>
+          <button onClick={() => updateCalc("5")}> 5 </button>
+          <button onClick={() => updateCalc("6")}> 6 </button>
+          <button onClick={() => updateCalc("7")}> 7 </button>
+          <button onClick={() => updateCalc("8")}> 8 </button>
+          <button onClick={() => updateCalc(".")}> . </button>
+          <button onClick={() => updateCalc("9")}> 9 </button>
+          <button onClick={clear}> CLEAR </button>
+        </div>
+      </div>
     </div>
   );
 }
